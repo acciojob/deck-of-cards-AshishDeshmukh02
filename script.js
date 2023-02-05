7874289bc3e0d5289af2c2e88afbfcc6d150eac4
Ashish Deshmukh <115146337+AshishDeshmukh02@users.noreply.github.com>
2023-02-05 16:11:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
 
+const deck = document.querySelector('#deck');
+const cards = deck.querySelectorAll('.whitebox2');
+
+function shuffleCards() {
+  for (let i = cards.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    deck.insertBefore(cards[j], cards[i]);
+    deck.insertBefore(cards[i], cards[j]);
+  }
+}
+
+shuffleCards();
